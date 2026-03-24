3c39a75ad0d619e1c694e1fc01db31f98f9a33a3
GoldyChoudhary00112 <ioc361162@gmail.com>
2026-03-24 23:47:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
-import { supabase } from "./supabase"
+import { supabase } from "./supabase";
 /* ─────────────────── CONSTANTS ─────────────────── */
 const LOCATIONS = [
   { id: 'football', label: 'Football Ground', emoji: '⚽' },
