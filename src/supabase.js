2651e19fab177cf691e2bee8045f834f62c276cb
GoldyChoudhary00112 <ioc361162@gmail.com>
2026-03-25 00:04:46 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl  = import.meta.env.https://oqodalrpjusbukkohgvp.supabase.co
-const supabaseKey  = import.meta.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xb2RhbHJwanVzYnVra29oZ3ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMzU2NDEsImV4cCI6MjA4OTkxMTY0MX0.NNO0YXTW9Uj2lmdAVWZHoDo5vV7w-pFjLQGhhhTj4Yc
+const supabaseUrl  = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey  = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 if (!supabaseUrl || !supabaseKey) {
   console.error('❌ Missing Supabase env vars. Check your .env file.')
